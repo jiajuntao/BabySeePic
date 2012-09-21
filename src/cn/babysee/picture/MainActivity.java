531854053ddddc74d04117d31b752a63b110475f
root <root@jiajuntao-Lenovo-Product.(none)>
2012-09-21 21:24:52 +0800

    add about info

--- src/cn/babysee/picture/MainActivity.java
+++ src/cn/babysee/picture/MainActivity.java
@@ -1,30 +1,39 @@
 package cn.babysee.picture;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-
 public class MainActivity extends Activity implements OnClickListener {
-    
+
+    private boolean DEBUG = AppEnv.DEBUG;
+    private String TAG = "MainActivity";
+
     private View title;
     private View view1;
     private View view2;
     private View view3;
     private View view4;
-    
+
     private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         context = getApplicationContext();
-        
+
         title = findViewById(R.id.title);
         title.setOnClickListener(this);
         view1 = findViewById(R.id.item_1);
@@ -40,24 +49,75 @@ public class MainActivity extends Activity implements OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.title:
-//                startActivity(new Intent(context, WebViewActivity.class));
-                break;
-            case R.id.item_1:
-                startActivity(new Intent(context, ViewPicActivity.class).putExtra("item", 0));
-                break;
-            case R.id.item_2:
-                startActivity(new Intent(context, ViewPicActivity.class).putExtra("item", 1));
-                break;
-            case R.id.item_3:
-                startActivity(new Intent(context, ViewPicActivity.class).putExtra("item", 2));
-                break;
-            case R.id.item_4:
-                startActivity(new Intent(context, ViewPicActivity.class).putExtra("item", 3));
-                break;
+        case R.id.title:
+            //                startActivity(new Intent(context, WebViewActivity.class));
+            break;
+        case R.id.item_1:
+            startActivity(new Intent(context, ViewPicActivity.class).putExtra("item", 0));
+            break;
+        case R.id.item_2:
+            startActivity(new Intent(context, ViewPicActivity.class).putExtra("item", 1));
+            break;
+        case R.id.item_3:
+            startActivity(new Intent(context, ViewPicActivity.class).putExtra("item", 2));
+            break;
+        case R.id.item_4:
+            startActivity(new Intent(context, ViewPicActivity.class).putExtra("item", 3));
+            break;
 
-            default:
-                break;
+        default:
+            break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        if (DEBUG)
+            Log.d(TAG, "onMenuItemSelected");
+
+        switch (item.getItemId()) {
+        case R.id.menu_about:
+            showDialog(0);
+            break;
+        case R.id.menu_advice:
+
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri maiUri = Uri.parse("mailto:babyseepic@gmail.com");
+                intent.setData(maiUri);
+                startActivity(Intent.createChooser(intent, getString(R.string.menu_advice)));
+            } catch (Exception e) {
+                if (DEBUG)
+                    e.printStackTrace();
+            }
+            break;
+        case R.id.menu_settings:
+
+            break;
+
+        default:
+            break;
+        }
+
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        View view = View.inflate(this, R.layout.about, null);
+
+        return new AlertDialog.Builder(this).setIcon(R.drawable.ic_launcher).setTitle(R.string.app_version)
+                .setView(view).setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dismissDialog(0);
+                    }
+                }).create();
     }
 }
